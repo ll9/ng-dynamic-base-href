4b205f67b732f6e84a5528e5ac0296ed26f0468c
ll9 <lwierer@web.de>
2024-05-23 16:31:08 +0200

    wp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'news',
+        loadComponent: () => import('./features/news/news-list/news-list.component').then(m => m.NewsListComponent)
+    },
+    {
+        path: 'news/:slug',
+        loadComponent: () => import('./features/news/news-detail/news-detail.component').then(m => m.NewsDetailComponent)
+    }
+];
